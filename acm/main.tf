d07a424f98de3dc8b72c75bdd37fadd574be9a3b
Your Name <az.okolo.2084@gmail.com>
2025-03-26 00:18:00 +0000

    updated domain name in acm

--- acm/main.tf
+++ acm/main.tf
@@ -11,7 +11,7 @@ resource "aws_acm_certificate" "acm_certificate" {
 
 # get details about a route 53 hosted zone
 data "aws_route53_zone" "route53_zone" {
-  name = var.domain_name
+  name = "trainiumcloud.com"
   private_zone = false 
 }
 
