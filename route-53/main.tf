d9c37730ded91727e81163e9592e4c0b04ed1a37
Your Name <az.okolo.2084@gmail.com>
2025-03-29 07:16:37 +0000

    updated record name

--- route-53/main.tf
+++ route-53/main.tf
@@ -6,7 +6,7 @@ data "aws_route53_zone" "hosted_zone" {
 # create a record set in route 53
 resource "aws_route53_record" "site_domain" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name = var.domain_name
+  name = var.record_name
   type = "A"
 
   alias {
